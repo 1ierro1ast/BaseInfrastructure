127a9862fc05aaecee185f88af58509dab0599ee
Muxa <gilfmuxa@gmail.com>
2022-10-25 11:12:31 +0400

    add base popup animation

---
+++ Assets/Codebase/Core/UI/PopupAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Codebase.Core.UI
+{
+    [RequireComponent(typeof(Animation))]
+    public class PopupAnimation : BasePopupAnimation
+    {
+        private Animation _animation;
+
+        protected override void OnInitialization()
+        {
+            _animation = GetComponent<Animation>();
+        }
+    }
+}
