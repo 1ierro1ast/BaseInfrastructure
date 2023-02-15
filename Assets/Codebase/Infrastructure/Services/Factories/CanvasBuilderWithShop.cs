9fc20f273faa7b40000ffc6c7780743d93c38419
Muxa <gilfmuxa@gmail.com>
2023-02-15 11:10:25 +0400

    add builder

---
+++ Assets/Codebase/Infrastructure/Services/Factories/CanvasBuilderWithShop.cs
@@ -0,0 +1,49 @@
+﻿using Codebase.Core.UI.Popups;
+using Codebase.Infrastructure.Services.AssetManagement;
+
+namespace Codebase.Infrastructure.Services.Factories
+{
+    public class CanvasBuilderWithShop : CanvasBuilderBase
+    {
+        public CanvasBuilderWithShop(IAssetProvider assetProvider) : base(assetProvider)
+        {
+            CreateCanvas(UIPath.MainCanvas);
+        }
+
+        public override CanvasBuilderBase BuildStartPopup()
+        {
+            CreatePopup<StartPopup>(UIPath.StartPopup);
+            return this;
+        }
+
+        public override CanvasBuilderBase BuildOverlayPopup()
+        {
+            CreatePopup<OverlayPopup>(UIPath.OverlayPopup);
+            return this;
+        }
+
+        public override CanvasBuilderBase BuildLosePopup()
+        {
+            CreatePopup<LosePopup>(UIPath.LosePopup);
+            return this;
+        }
+
+        public override CanvasBuilderBase BuildWinPopup()
+        {
+            CreatePopup<WinPopup>(UIPath.WinPopup);
+            return this;
+        }
+
+        public override CanvasBuilderBase BuildShopPopup()
+        {
+            CreatePopup<ShopPopup>(UIPath.ShopPopup);
+            return this;
+        }
+
+        private void CreatePopup<T>(string path) where T : PopupBase
+        {
+            var popup = assetProvider.Instantiate<T>(path);
+            MainCanvas.Add(popup);
+        }
+    }
+}
