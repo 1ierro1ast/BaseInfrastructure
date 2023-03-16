df3a04dcb2e944ea494c5adc00e7ee07422b6f74
HoSHIZA <needahentai@gmail.com>
2023-03-16 23:24:33 +0300

    Reworked EventBus to an improved version that supports parameter transfer

--- Assets/Codebase/Infrastructure/GameFlow/EventBusSystem/EventBus.cs
+++ Assets/Codebase/Infrastructure/GameFlow/EventBusSystem/EventBus.cs
@@ -3,72 +3,74 @@ using System.Collections.Generic;
 
 namespace Codebase.Infrastructure.GameFlow.EventBusSystem
 {
-    public class EventBus : IEventBus
+    public sealed class EventBus : IEventBus
     {
-        private Dictionary<Type, List<Action>> _subscribers;
-        private bool _firing;
-        private List<int> _idsForRemove;
-
-        public EventBus()
+        private readonly Dictionary<Type, List<Delegate>> _subscribers = new Dictionary<Type, List<Delegate>>();
+        
+        public void Subscribe<T>(Action<T> handler) where T : IEvent
         {
-            _subscribers = new Dictionary<Type, List<Action>>();
-            _idsForRemove = new List<int>(100);
+            var eventType = typeof(T);
+            if (!_subscribers.ContainsKey(eventType))
+            {
+                _subscribers[eventType] = new List<Delegate>();
+            }
+
+            _subscribers[eventType].Add(handler);
         }
 
-        public void Subscribe<T>(Action callback) where T : IEvent
+        public void Subscribe<T>(Action handler) where T : IEvent
         {
-            if (_subscribers.ContainsKey(typeof(T)))
+            var eventType = typeof(T);
+            if (!_subscribers.ContainsKey(eventType))
             {
-                _subscribers[typeof(T)].Add(callback);
+                _subscribers[eventType] = new List<Delegate>();
             }
-            else
-            {
-                _subscribers.Add(typeof(T), new List<Action>{callback});
-            }
+
+            _subscribers[eventType].Add(handler);
         }
 
-        public void Unsubscribe<T>(Action callback) where T : IEvent
+        public void Unsubscribe<T>(Action<T> handler) where T : IEvent
         {
-            if (!_subscribers.ContainsKey(typeof(T))) return;
+            var eventType = typeof(T);
+            if (!_subscribers.ContainsKey(eventType)) return;
 
-            var subscribedMethods = _subscribers[typeof(T)];
+            _subscribers[eventType].Remove(handler);
+        }
 
-            if (!subscribedMethods.Contains(callback)) return;
+        public void Unsubscribe<T>(Action handler) where T : IEvent
+        {
+            var eventType = typeof(T);
+            if (!_subscribers.ContainsKey(eventType)) return;
 
-            if (!_firing) subscribedMethods.Remove(callback);
-            else
-            {
-                subscribedMethods[subscribedMethods.IndexOf(callback)] = null;
-            }
+            _subscribers[eventType].Remove(handler);
         }
 
         public void Fire<T>() where T : IEvent
         {
-            if (!_subscribers.ContainsKey(typeof(T))) return;
-            _firing = true;
-            var actionsCache = new List<Action>();
-
-            actionsCache.AddRange(_subscribers[typeof(T)]);
-
-            foreach (var item in actionsCache)
-            {
-                if (item == null)
-                {
-                    _idsForRemove.Add(actionsCache.IndexOf(item));
-                    continue;
-                }
-
-                item.Invoke();
-            }
-
-            _firing = false;
+            IEvent instance = Activator.CreateInstance<T>();
             
-            foreach (var id in _idsForRemove)
-            {
-                _subscribers[typeof(T)].RemoveAt(id);
-            }
+            var eventType = instance.GetType();
+            
+            if (!_subscribers.ContainsKey(eventType)) return;
 
-            _idsForRemove.Clear();
+            var handlers = _subscribers[eventType].ToArray();
+            foreach (var handler in handlers)
+            {
+                handler.DynamicInvoke();
+            }
+        }
+        
+        public void Fire(IEvent eventToFire)
+        {
+            var eventType = eventToFire.GetType();
+            
+            if (!_subscribers.ContainsKey(eventType)) return;
+
+            var handlers = _subscribers[eventType].ToArray();
+            foreach (var handler in handlers)
+            {
+                handler.DynamicInvoke(eventToFire);
+            }
         }
     }
 }
